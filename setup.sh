9a95f2a9561f41825db7fabea1ce3eb704a5f55c
Elwyn John <elwynjohn919@gmail.com>
2022-12-27 03:36:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 abspath=$(cd -- $(dirname -- ${BASH_SOURCE[0]}) && pwd)
 
-sh -c 'curl -fLo "${abspath}"/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "curl -fLo ${abspath}/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+if [[ $? != 0 ]]; then
+  echo "error curling plug.vim"
+  exit 1
+fi
 
+rm ~/.config/nvim
 ln -s "${abspath}/nvim" ~/.config/nvim
 # nvim -S nvim_setup.lua

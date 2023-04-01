0936aaba32a4b1e26a1f9efdbb6f8a7ac27761d3
John <elwyn.john@accenture.com>
2023-04-01 22:52:41 +0100

    Add Winter settings

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,8 @@
 $abspath = split-path -parent $MyInvocation.MyCommand.Definition
 
-New-Item -Path ~\AppData\local\nvim -ItemType SymbolicLink -Value ${abspath}/nvim
+New-Item -Force -Path ~\AppData\local\nvim -ItemType SymbolicLink -Value ${abspath}/nvim
+New-Item -Force -Path ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -ItemType SymbolicLink -Value ${abspath}/settings.json
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+

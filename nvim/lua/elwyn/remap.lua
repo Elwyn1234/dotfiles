e3b992301dcb0570dc312624b0b6c01dc76f0eb2
John <elwyn.john@accenture.com>
2023-04-01 19:10:25 +0100

    Buffer, fold, and :Ex mappings

--- nvim/lua/elwyn/remap.lua
+++ nvim/lua/elwyn/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
 -- These allow you to move selections
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -29,3 +29,10 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true}) -- can
 -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Buffer next
+vim.keymap.set("n", "<leader>bn", "<cmd>bn<CR>")
+vim.keymap.set("n", "<leader>bp", "<cmd>bp<CR>")
+
+-- Fold class or Go function
+vim.keymap.set("n", "<leader>zc", "/class<CR>zfa{")
+vim.keymap.set("n", "<leader>zf", "/func<CR>zfa{")
